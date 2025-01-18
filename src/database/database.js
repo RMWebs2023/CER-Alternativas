59235fa098624fd0c3beeef6f6f067e2003a65ae
René <107271569+renevalderrey@users.noreply.github.com>
2025-01-18 17:41:04 -0300

    lógica de cards

---
+++ src/database/database.js
@@ -0,0 +1,29 @@
+const offers = [
+    {
+        name: "Test 1",
+        description: "Esto es una descripción",
+        experience: "Experiencia laboral",
+    },
+    {
+        name: "Test 1",
+        description: "Esto es una descripción",
+        experience: "Experiencia laboral",
+    },
+    {
+        name: "Test 1",
+        description: "Esto es una descripción",
+        experience: "Experiencia laboral",
+    },
+    {
+        name: "Test 1",
+        description: "Esto es una descripción",
+        experience: "Experiencia laboral",
+    },
+    {
+        name: "Test 1",
+        description: "Esto es una descripción",
+        experience: "Experiencia laboral",
+    },
+]
+
+export default offers;
